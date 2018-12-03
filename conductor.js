895d39f18bd3b0de86d95d6c4d4d67d27add4f1e
chen650 <chen650@usc.edu>
2018-12-02 17:41:21 -0800

    gift box and forest

---
+++ conductor.js
@@ -0,0 +1,50 @@
+/*
+This file is the orchestral conductor for the animation, coordinating what plays when.
+
+For each <section> (with class="step") in the index.html file, this file contains:
+   (1) an "activate" function, which is called when the scrolled position switches sections, and
+   (2) an "update" function, which is called when the scrolled position changes within a section
+
+Note that the "update" functions are functions of "progress", which is the relative position
+within the section
+*/
+
+var activateFunctions = []
+var updateFunctions = []
+var reverseFunctions = []
+
+
+/* Title Section */
+
+for (i=0; i<document.getElementById('sections').childElementCount; i++) {
+  activateFunctions.push( function() {})
+  updateFunctions.push( function() {})
+  reverseFunctions.push( function() {})
+}
+
+activateFunctions[0] = function(){ 
+  graphic.style.opacity = 0;
+}
+
+activateFunctions[1] = function(){ 
+  $('#graphic').fadeTo('slow', 1);
+  $('#header').fadeTo('slow', 0);
+};
+
+activateFunctions[2] = function(){ 
+  $('body').animate({backgroundColor:'#add8e6'}, 300);
+  $('.step').animate({color:'#000000'}, 300);
+};
+
+reverseFunctions[0] = function(){ 
+  $('#header').fadeTo('slow', 1);
+  $('#graphic').fadeTo('slow', 0);
+};
+
+reverseFunctions[1] = function(){ 
+  $('body').animate({backgroundColor:'#000000'}, 300);
+  $('.step').animate({color:'#fdc56d'}, 300);
+};
+
+// Make it happen!
+scrollerDisplay(d3.select('#graphic'), 'step', activateFunctions, updateFunctions)
---
+++ scrollerDisplay.js
@@ -0,0 +1,61 @@
+
+// from Jim Vallandingham's scrollytelling with D3 code, with minor tweaks
+// https://github.com/vlandham/scroll_demo/blob/gh-pages/js/sections.js
+// (in particular, see lines 765-793)
+
+
+function scrollerDisplay(container, stepClass, activateFunctions, updateFunctions) {
+
+  var lastIndex = -1
+  var activeIndex = 0
+
+  var scroll = scroller()
+    .container(container)
+
+  scroll(d3.selectAll('.' + stepClass))
+
+  scroll.on('active', function(index) {
+
+    activeIndex = index
+    var sign = (activeIndex - lastIndex) < 0 ? -1 : 1
+    var scrolledSections = d3.range(lastIndex + sign, activeIndex + sign, sign)
+    scrolledSections.forEach(function(i) {
+      if(isScrollUp === false) {
+        activateFunctions[i]() // doesn't matter if number of functions exceeds sections
+      } else {
+        reverseFunctions[i]()
+      }
+    })
+    lastIndex = activeIndex
+
+    d3.selectAll('.' + stepClass)
+      .attr('class', function(d,i) {
+          return (i == index) ? stepClass + ' highlighted-step' : stepClass
+        })
+
+  })
+
+  scroll.on('progress', function(index, progress){
+    updateFunctions[index](progress)
+  })
+
+}
+
+// detect whether user is scrolling up or scrolling down
+var lastScrollTop = 0;
+var isScrollUp = false;
+$(window).scroll(function(event){
+   var scrollTop = $(this).scrollTop();
+   if (scrollTop > lastScrollTop){
+       isScrollUp = false;
+   }
+   else if(scrollTop == lastScrollTop)
+   {
+     //do nothing 
+     //In IE this is an important condition because there seems to be some instances where the last scrollTop is equal to the new one
+   }
+   else {
+      isScrollUp = true;
+   }
+   lastScrollTop = scrollTop;
+});
